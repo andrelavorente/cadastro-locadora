9da7f0d61ee9e637e726aaedaa06ecb43bc0f16b
andrelavorente <lavorente.a@gmail.com>
2023-09-04 14:46:13 -0300

    att

--- backend/modelo/Cliente.js
+++ backend/modelo/Cliente.js
@@ -1,3 +1,3 @@
 export default class Cliente {
-  constructor(cpf, nome, endereco, bairro, cidade, estado, telefone, email) {}
+  constructor(nome, sobrenome, endereco, cpf, cidade, telefone, email) {}
 }
